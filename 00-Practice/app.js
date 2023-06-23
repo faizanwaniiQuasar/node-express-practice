2f6e9478513337810603bddd8537392a5df456ba
Faizan Wani <fzwani@dc.iquasar.com>
2023-06-23 16:11:12 +0530

    add practice

---
+++ 00-Practice/app.js
@@ -0,0 +1,9 @@
+const secret = "Top secret";
+const adil = "Adil";
+const faisal = "Faisal";
+
+const sayHi = function (name) {
+  console.log(`Hello there my name is ${name}`);
+};
+
+sayHi("arbaz");
